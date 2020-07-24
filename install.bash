0155bab60510eb339a906b55d34749c01164b1cb
xortrike <44906253+xortrike@users.noreply.github.com>
2020-07-24 11:50:20 +0300

    Add MailHog container, SMTP server.

--- install.bash
+++ install.bash
@@ -2,84 +2,87 @@
 
 function CreateDirectory
 {
-    local dir=$1
-    local directory=(
-        [0]="docker"
-        [1]="logs"
-        [2]="logs/nginx"
-        [3]="logs/mysql"
-        [4]="mysql"
-        [5]="mysql/data"
-        [6]="mysql/dump"
-        [7]="www"
-    )
-    local permission=(
-        [0]=""
-        [1]="docker"
-        [2]="docker"
-        [3]="docker"
-        [4]="docker"
-        [5]="docker"
-        [6]="docker"
-        [7]="www-data"
-    )
-    for i in "${!directory[@]}"
+	local dir=$1
+	local directory=(
+		[0]="docker"
+		[1]="logs"
+		[2]="logs/nginx"
+		[3]="logs/mysql"
+		[4]="mysql"
+		[5]="mysql/data"
+		[6]="mysql/dump"
+		[7]="www"
+	)
+	local permission=(
+		[0]=""
+		[1]="docker"
+		[2]="docker"
+		[3]="docker"
+		[4]="docker"
+		[5]="docker"
+		[6]="docker"
+		[7]="www-data"
+	)
+	for i in "${!directory[@]}"
 	do
-        if [ -d "${dir}/${directory[$i]}" ]
-        then
-            echo "Directory skip: ${directory[$i]}"
-        else
-		    echo "Create directory: ${directory[$i]}"
-            mkdir -p "${dir}/${directory[$i]}"
-            if [[ ! -z ${permission[$i]} ]]
-            then
-                sudo chmod -R 775 "${dir}/${directory[$i]}"
-                sudo chown -R $USER:${permission[$i]} "${dir}/${directory[$i]}"
-            fi
-        fi
+		if [ -d "${dir}/${directory[$i]}" ]
+		then
+			echo "Directory skip: ${directory[$i]}"
+		else
+			echo "Create directory: ${directory[$i]}"
+			mkdir -p "${dir}/${directory[$i]}"
+			if [[ ! -z ${permission[$i]} ]]
+			then
+				sudo chmod -R 775 "${dir}/${directory[$i]}"
+				sudo chown -R $USER:${permission[$i]} "${dir}/${directory[$i]}"
+			fi
+		fi
 	done
 }
 
 function setCertificate
 {
-    cd ${path}"/docker/nginx/openssl"
+	cd ${path}"/docker/nginx/openssl"
 
-    if [ -z "$1" ]; then
-        echo "Please supply a subdomain to create a certificate for";
-        echo "e.g. mysite.localhost"
-        exit;
-    fi
+	if [ -z "$1" ]; then
+		echo "Please supply a subdomain to create a certificate for";
+		echo "e.g. mysite.localhost"
+		exit;
+	fi
 
-    if [ -f ${path}"/docker/nginx/cert/$1" ]; then
-        rm -rf ${path}"/docker/nginx/cert/$1"
-    fi
+	if [ -f device.key ]; then
+		KEY_OPT="-key"
+	else
+		KEY_OPT="-keyout"
+	fi
 
-    if [ -f device.key ]; then
-        KEY_OPT="-key"
-    else
-        KEY_OPT="-keyout"
-    fi
+	DOMAIN=$1
+	COMMON_NAME=${2:-$1}
 
-    DOMAIN=$1
-    COMMON_NAME=${2:-$1}
+	if [ -f $DOMAIN ]; then
+		rm -rf $DOMAIN
+	fi
 
-    SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=$COMMON_NAME"
-    NUM_OF_DAYS=999
+	SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=$COMMON_NAME"
+	NUM_OF_DAYS=999
 
-    openssl req -new -newkey rsa:2048 -sha256 -nodes $KEY_OPT device.key -subj "$SUBJECT" -out device.csr
+	openssl req -new -newkey rsa:2048 -sha256 -nodes $KEY_OPT device.key -subj "$SUBJECT" -out device.csr
 
-    cat v3.ext | sed s/%%DOMAIN%%/$COMMON_NAME/g > /tmp/__v3.ext
+	cat v3.ext | sed s/%%DOMAIN%%/$COMMON_NAME/g > /tmp/__v3.ext
 
-    openssl x509 -req -in device.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out device.crt -days $NUM_OF_DAYS -sha256 -extfile /tmp/__v3.ext
+	openssl x509 -req -in device.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out device.crt -days $NUM_OF_DAYS -sha256 -extfile /tmp/__v3.ext
 
-    mkdir $DOMAIN
+	mkdir $DOMAIN
 
-    mv device.csr $DOMAIN/$DOMAIN.csr
-    mv device.crt $DOMAIN/$DOMAIN.crt
-    cp device.key $DOMAIN/$DOMAIN.key
+	mv device.csr $DOMAIN/$DOMAIN.csr
+	mv device.crt $DOMAIN/$DOMAIN.crt
+	cp device.key $DOMAIN/$DOMAIN.key
 
-    mv $DOMAIN "../cert"
-    cd ${path}
+	if [ ! -f "../cert" ]; then
+		mkdir -p "../cert"
+	fi
+	mv $DOMAIN "../cert"
+	cd ${path}
 }
 
 path=$PWD
@@ -94,13 +97,13 @@ config["host_name"]="example.local"
 # Project Name
 read -p "Enter Project name [${config["project_name"]}]:" buffer
 if [ ! -z $buffer ]; then
-    config["project_name"]=$buffer
+	config["project_name"]=$buffer
 fi
 
 # Site Address
 read -p "Enter Site Address [${config["host_name"]}]:" buffer
 if [ ! -z $buffer ]; then
-    config["host_name"]=$buffer
+	config["host_name"]=$buffer
 fi
 
 # Before start install
@@ -109,19 +112,19 @@ echo " - Project Name: ${config["project_name"]}"
 echo " - Site Address: ${config["host_name"]}"
 read -p "Start to install? [y/n]: " buffer
 if [ -z $buffer ] || [ $buffer != "y" ]; then
-    exit
+	exit
 fi
 
 
 
-echo "Installing..."
+echo -e "\nInstalling..."
 # Step 1 - Create Directory
 CreateDirectory $path
 
 # Step 2 - Extract Archive
 if [ ! -f "${path}/${name}" ]; then
-    echo "Error! Archive ${name} not found."
-    exit
+	echo "Error! Archive ${name} not found."
+	exit
 fi
 tar xvC "${path}/docker" -f "${path}/${name}"
 
@@ -132,6 +135,7 @@ setCertificate ${config["host_name"]}
 search="example.local"
 replace=${config["host_name"]}
 sed -i "s/${search}/${replace}/g" "${path}/docker/nginx/magento.conf"
+sed -i "s/${search}/${replace}/g" "${path}/docker/nginx/joomla.conf"
 
 search="HOST_NAME=example.local"
 replace="HOST_NAME=${config["host_name"]}"
